4f72a8564dcf8fb3cdd1430216f5df998324d0f9
kaadon <kaadon@foxmail.com>
2021-09-08 21:38:31 +0800

    初次提交

---
+++ src/TronService/Credential.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Kaadon\Tron\TronService;
+
+
+use Elliptic\EC;
+use kornrunner\Keccak;
+
+class Credential
+{
+    protected $keyPair;
+
+    public function __construct($privateKey)
+    {
+        $ec            = new EC('secp256k1');
+        $this->keyPair = $ec->keyFromPrivate($privateKey);
+    }
+
+    public static function fromPrivateKey($privateKey)
+    {
+        return new self($privateKey);
+    }
+
+    public static function create()
+    {
+        $bin        = 'JSJ' . random_bytes(32);
+        $privateKey = bin2hex($bin);
+        return new self($privateKey);
+    }
+
+    public function privateKey()
+    {
+        return $this->keyPair->getPrivate()->toString(16, 2);
+    }
+
+    public function publicKey()
+    {
+        return $this->keyPair->getPublic()->encode('hex');
+    }
+
+    public function address()
+    {
+        return Address::fromPublicKey($this->publicKey());
+    }
+
+    public function sign($hex)
+    {
+        $signature = $this->keyPair->sign($hex);
+        $r         = $signature->r->toString('hex');
+        $s         = $signature->s->toString('hex');
+        //$v = bin2hex(pack('C',$signature->recoveryParam));
+        $v = bin2hex(chr($signature->recoveryParam));
+        return $r . $s . $v;
+    }
+
+    public function signTx($tx)
+    {
+        $signature = $this->sign($tx->txID);
+        //var_dump($signature);
+        $tx->signature = [$signature];
+        return $tx;
+    }
+}
---
+++ src/TronService/NodeClient.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Kaadon\Tron\TronService;
+
+
+use GuzzleHttp\Client;
+
+class NodeClient
+{
+    protected $client;
+
+    public static function mainNet()
+    {
+        return new self('http://13.124.62.58:8090');
+    }
+
+    public static function testNet()
+    {
+        return new self('https://api.shasta.trongrid.io');
+    }
+
+    public function __construct($uri)
+    {
+        $opts         = [
+            'base_uri' => $uri,
+        ];
+        $this->client = new Client($opts);
+    }
+
+    public function post($api, $payload = [])
+    {
+        $opts = [
+            'json' => $payload,
+        ];
+        $rsp  = $this->client->post($api, $opts);
+        return $this->handle($rsp);
+    }
+
+    public function get($api, $query = [])
+    {
+        $opts = [
+            'query' => $query,
+        ];
+        $rsp  = $this->client->get($api, $opts);
+        return $this->handle($rsp);
+    }
+
+    public function handle($rsp)
+    {
+        $content = $rsp->getBody();
+        //echo $content . PHP_EOL;
+        return json_decode($content);
+    }
+
+    public function version()
+    {
+        return '1.0.0';
+    }
+}
