95db51c5bc97bb1e7069e61621f2f84ed652e184
Kaadon <kaadon@foxmail.com>
2021-09-26 01:04:59 +0800

    修复地址创建

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 
 require "vendor/autoload.php";
 
-$a = \Kaadon\TronService\Credential::create()->address();
+$a = \Kaadon\TronService\Credential::create()->privateKey();
 var_dump($a);
+$a = \Kaadon\TronService\Credential::fromPrivateKey('0880d6667c5bdc79af33c4e7df15e3f22fd2ab9cbd4d5d14e863e08504f1c509');
