0f234f2ef42bab51c4b4f36eff17d39bbbe1fe3b
kaadon <kaadon@foxmail.com>
2021-10-07 11:40:47 +0800

    updateMUNU

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require "vendor/autoload.php";
-
-$a = \Kaadon\TronService\Credential::create()->privateKey();
-var_dump($a);
-$a = \Kaadon\TronService\Credential::fromPrivateKey('0880d6667c5bdc79af33c4e7df15e3f22fd2ab9cbd4d5d14e863e08504f1c509');
